d349548a8c3c051d8e4f593a76ce7f9d635abf87
Michel John <michel.santos@dataside.com.br>
2026-05-11 18:16:06 -0300

    feat: implement SignUp use case with bcrypt password hashing and error handling

---
+++ src/application/useCases/SignUpUseCase.ts
@@ -0,0 +1,25 @@
+import {hash} from "bcryptjs";
+
+import { AccountAlreadyExists } from "../erros/AccountAlreadyExists.js";
+import { prismaClient } from "../libs/prismaClient.js";
+
+const SALT = 10;
+
+interface IInput {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export class SignUpUseCase {
+  async execute({ name, email, password }: IInput) {
+
+    const account = await prismaClient.account.count({ where: { email } });
+
+    if (account){throw new AccountAlreadyExists}
+
+    const hashedPassword = await hash(password, SALT);
+
+    await prismaClient.account.create({data:{name, email, password:hashedPassword}})
+  }
+}

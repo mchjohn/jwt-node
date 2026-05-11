ac31f4e1ff2e50611d298a5adaa1498c7f751fae
Michel John <michel.santos@dataside.com.br>
2026-05-11 19:14:01 -0300

    feat: add SignIn and SignUp controllers with Zod validation and error handling

---
+++ src/application/controllers/SignInController.ts
@@ -0,0 +1,49 @@
+import z, { ZodError } from "zod";
+import { InvalidCrendentials } from "../erros/InvalidCrendentials.js";
+import type {
+  IController,
+  IRequest,
+  IResponse,
+} from "../interfaces/IController.js";
+import type { SignInUseCase } from "../useCases/SignInUseCase.js";
+
+const schema = z.object({
+  email: z.email(),
+  password: z.string().min(6),
+});
+
+export class SignInController implements IController {
+  constructor(private readonly signInUseCase: SignInUseCase) {}
+
+  async handle({ body }: IRequest): Promise<IResponse> {
+    try {
+      const { email, password } = schema.parse(body);
+
+      const { accessToken } = await this.signInUseCase.execute({
+        email,
+        password,
+      });
+
+      return {
+        statusCode: 200,
+        body: { accessToken },
+      };
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return {
+          statusCode: 400,
+          body: error.issues,
+        };
+      }
+
+      if (error instanceof InvalidCrendentials) {
+        return {
+          statusCode: 401,
+          body: { error: "Invalid credentials." },
+        };
+      }
+
+      throw error;
+    }
+  }
+}
---
+++ src/application/controllers/SignUpController.ts
@@ -0,0 +1,47 @@
+import z, { ZodError } from "zod";
+import { AccountAlreadyExists } from "../erros/AccountAlreadyExists.js";
+import type {
+  IController,
+  IRequest,
+  IResponse,
+} from "../interfaces/IController.js";
+import type { SignUpUseCase } from "../useCases/SignUpUseCase.js";
+
+const schema = z.object({
+  name: z.string().min(2),
+  email: z.email(),
+  password: z.string().min(6),
+});
+
+export class SignUpController implements IController {
+  constructor(private readonly signUpUseCase: SignUpUseCase) {}
+
+  async handle({ body }: IRequest): Promise<IResponse> {
+    try {
+      const { name, email, password } = schema.parse(body);
+
+      await this.signUpUseCase.execute({ name, email, password });
+
+      return {
+        statusCode: 204,
+        body: null,
+      };
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return {
+          statusCode: 400,
+          body: error.issues,
+        };
+      }
+
+      if (error instanceof AccountAlreadyExists) {
+        return {
+          statusCode: 409,
+          body: { error: "Email already in use." },
+        };
+      }
+
+      throw error;
+    }
+  }
+}

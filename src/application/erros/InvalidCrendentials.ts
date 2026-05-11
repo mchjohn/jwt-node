a890bbbe36fb7cb3c0bd7f7c26309840a5828a6a
Michel John <michel.santos@dataside.com.br>
2026-05-11 18:42:54 -0300

    feat: implement SignIn use case with JWT token generation and error handling

---
+++ src/application/erros/InvalidCrendentials.ts
@@ -0,0 +1,3 @@
+export class InvalidCrendentials extends Error{
+  name = "InvalidCrendentials"
+}
---
+++ src/application/useCases/SignInUseCase.ts
@@ -0,0 +1,43 @@
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { ENV } from "../../config/env.js";
+import { InvalidCrendentials } from "../erros/InvalidCrendentials.js";
+import { prismaClient } from "../libs/prismaClient.js";
+
+interface IInput {
+  email: string;
+  password: string;
+}
+
+interface IOutput {
+  accessToken: string;
+}
+
+export class SignInUseCase {
+  async execute({ email, password }: IInput): Promise<IOutput> {
+    const account = await prismaClient.account.findUnique({
+      where: { email },
+      select: { id: true, password: true },
+    });
+
+    if (!account) {
+      throw new InvalidCrendentials();
+    }
+
+    const validPassword = await compare(password, account.password);
+
+    if (!validPassword) {
+      throw new InvalidCrendentials();
+    }
+
+    const accessToken = sign(
+      {
+        sub: account.id,
+      },
+      ENV.JWT_SECRET,
+      { expiresIn: "2d" },
+    );
+
+    return { accessToken };
+  }
+}
